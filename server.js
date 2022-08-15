5d837e12ba03e734bba43b6cb5846cacac3d661c
haruto17 <s1902272@sendai-nct.jp>
2022-08-15 16:59:25 +0900

    計算式の追加

--- server.js
+++ server.js
@@ -3,10 +3,22 @@ import { serveDir } from "https://deno.land/std@0.151.0/http/file_server.ts";
 
 serve(async (req) => {
   const pathname = new URL(req.url).pathname;
+
+  const maxlife = 100;
+  let life = 100;
+  let old = 20;
+  let temp = 30;
+  let humidity = 50;
+
   console.log(pathname);
 
   if (req.method === "GET" && pathname === "/welcome-message") {
     return new Response("ハルトマン");
+  }
+
+  if (req.method === "GET" && pathname === "/Life-gauge") {
+    life -= old * (temp + humidity / 100) * 0.01;
+    return new Response(life);
   }
 
   return serveDir(req, {
