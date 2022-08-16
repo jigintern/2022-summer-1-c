6b677724133c4f09405fc80493ed3742461fe244
ting <106464988+8658108@users.noreply.github.com>
2022-08-16 13:08:45 +0900

    Update server.js

--- server.js
+++ server.js
@@ -10,26 +10,41 @@ serve(async (req) => {
   }
 
   if (req.method === "GET" && pathname === "/location-information") {
-    navigator.geolocation.watchPosition( (position) => {
-      var lat = position.coords.latitude;
-      var lng = position.coords.longitude;
-      var accu = position.coords.accuracy;
-      
-      var txt = document.getElementById("txt");
-      txt.innerHTML = "緯度: " + lat + "<br>" 
-      + "経度: " + lng + "<br>"
-      + "精度: " + accu + "<br>";
+    const pos = await getCurrentPosition();
+    console.log(pos.coords);
+    const lat = position.coords.latitude;
+    const lng = position.coords.longitude;
+    const acc = position.coords.accuracy;
 
-      return new Response(txt);
-
-    },(error) =>{
-    },{
-      enableHighAccuracy: true
-    });
+    const txt = document.getElementById("txt"); // テスト用
+    txt.innerHTML = "緯度: " + lat + "<br>"   // 緯度
+                  + "経度: " + lng + "<br>"   // 経度
+                  + "精度: " + acc + "<br>"; // その精度
   }
 
+  const calcDistance = (lat1, lng1, lat2, lng2) => {
+    const R = Math.PI / 180;
+    lat1 *= R;
+    lng1 *= R;
+    lat2 *= R;
+    lng2 *= R;
+    return 6371000 * Math.acos(Math.cos(lat1) * Math.cos(lat2) * Math.cos(lng2 - lng1) + Math.sin(lat1) * Math.sin(lat2));
+    /* https://ja.wikipedia.org/wiki/大円距離 */
+  };
+
   if (req.method === "GET" && pathname === "/temperature-humidity") {
-    const url = "https://www.jma.go.jp/bosai/amedas/const/amedastable.json"
+    const amedasURL = "https://www.jma.go.jp/bosai/amedas/const/amedastable.json";
+    const generateDateimeString = () => {
+      const nowTime = new Date(); // 現在時刻を取得
+      nowTime.setHours((new Date()).getHours() -1); // 最終更新にHoursを調整
+      return nowTime.getFullYear().toString()                       // 年
+             + (nowTime.getMonth() + 1).toString().padStart(2, '0') // 月
+             + nowTime.getDate().toString().padStart(2, '0')        // 日
+             + nowTime.getHours().toString().padStart(2, '0')       // 時
+             + "0000";                                              // 分、秒
+    }
+    const queryDatetime = generateDateimeString()
+
   }
  
 
